51e479af2af4bbe6ee1a89e57a6334a07c2e363b
lazypool <xiao_xiao_pku@163.com>
2022-12-29 19:38:37 +0800

    change dmenufonts

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "nerd-fonts-complete:size=24" };
-static const char dmenufont[]       = "nerd-fonts-complete:size=24";
+static const char dmenufont[]       = "nerd-fonts-complete:size=48";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
