97f9e7d6ef9d23f303c208b568565f77b11ad257
lazypool <lazypool@proton.me>
2025-07-22 18:25:35 +0800

    添加 mem.sh

---
+++ DWM/scripts/status/mem.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 获取当前内存使用
+
+_this=_mem
+tmpfile=$(cd $(dirname $0);cd ..;pwd)/tmp
+
+update() {
+	mem_icon="  "
+	mem_tota=$(cat /proc/meminfo | grep -w "MemTotal:"| awk '{print $2}')
+	mem_free=$(cat /proc/meminfo | grep -w "MemFree:" | awk '{print $2}')
+	mem_buff=$(cat /proc/meminfo | grep -w "Buffers:" | awk '{print $2}')
+	mem_cach=$(cat /proc/meminfo | grep -w "Cached:"  | awk '{print $2}')
+	mem_text=$(echo $(((mem_tota - mem_free - mem_buff - mem_cach) * 100 / mem_tota)) | awk '{printf "%02d%", $1}')
+	sed -i '/^export '$_this'=.*$/d' $tmpfile
+	printf "export %s=' %s%s '\n" "$_this" "$mem_icon" "$mem_text" >> $tmpfile
+}
+
+notify() {
+	notify-send " Memory" "\n$(free -h | awk '
+		NR==2 {a=$7;u=$3;t=$2} 
+		NR==3 {s=$3"/"$2} 
+		END {printf "Free: %s\nUsed: %s/%s\nSwap: %s", a, u, t, s}')" -r 9527
+}
+
+click() {
+	case "$1" in
+		L) notify ;;
+		R) "" ;;
+	esac
+}
+
+case "$1" in
+	click) click $2 ;;
+	notify) notify ;;
+	*) update ;;
+esac
