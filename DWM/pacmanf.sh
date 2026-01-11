2c09b2c04ab0fa5aaf64236364a576465f851f8f
lazypool <lazypool@proton.me>
2026-01-11 21:39:22 +0800

    refactor(script): unify status tool windows and improve pacmanf.sh
    Update pacmanf.sh, replacing `sudo` to `su`, which is more robust.
    Refactor statusbar.h to kill other status tool processes (fetch, pacman,
    htop) before launching a new one, ensuring single instance operation.
    Simplify window geometry calculations by unifying position to UR0 with
    consitent dimensions for all status tolls. Remove unused geometry
    variables UR1 and UR2, and simplify screen dimension calculations.

--- DWM/pacmanf.sh
+++ DWM/pacmanf.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo -e '\033[0;32mtry to update, please enter password\033[0m'
-if sudo pacman -Syyu; then
+if su -c 'pacman -Syyu'; then
 	if ! checkupdates; then
 		printf '' >"$DWM/.tmp/pkgupdates.tmp"
 		echo -e '\033[0;32mupdate ok, enter any key to quit\033[0m'
 	fi
 else
-	echo -e '\033[0;32mupdate failed, please check the network\033[0m'
+	echo -e '\033[0;32mupdate failed, please check the network or password\033[0m'
 fi
 old_settings=$(stty -g)
 stty -icanon -echo
